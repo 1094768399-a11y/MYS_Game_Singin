7dde83afbfad9d39c2feec2e61d3807e96eca2b5
bailihai <76420852+bailihai@users.noreply.github.com>
2025-11-10 19:49:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { doCloudSign } from './src/MihoyoCloud/index.js'
 async function main() {
   await doMYSSign('Genshin')
   await doMYSSign('StarRail')
+  await doMYSSign('Zenless')  // 新增绝区零米游社签到
   await doCloudSign('CloudYS')
   await doCloudSign('CloudSR')
 }
